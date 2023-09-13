b4fb84f43c477d33d64c46c28835d2044f166152
unknown <vedanttandel120@gmail.com>
2023-09-13 14:49:39 +0530

    auth update 1.2

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ connectionDB();
 //   })
 
 app.options("/*", function(req, res, next){
-  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
-    // res.header('Access-Control-Allow-Origin', "http://127.0.0.1:3000");
+  // res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Origin', "http://127.0.0.1:3000");
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Content-Type","application/json");
